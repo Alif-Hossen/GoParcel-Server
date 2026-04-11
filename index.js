758a9bcbc665901e156e04f0ddc724929985917c
Alif-Hossen <naimuralif1.0@gmail.com>
2026-04-11 00:21:56 -0600

    Display Completed Task

--- index.js
+++ index.js
@@ -207,8 +207,11 @@ async function run() {
             if (riderEmail) {
                 query.riderEmail = riderEmail
             }
-            if (deliveryStatus) {
-                query.deliveryStatus = {$in: ['driver_assigned', 'rider_arriving']}
+            if (deliveryStatus !== 'parcel_delivered') {
+                // query.deliveryStatus = {$in: ['driver_assigned', 'rider_arriving']}
+                query.deliveryStatus = {$nin: ['parcel_delivered']}
+            } else {
+                query.deliveryStatus = deliveryStatus;
             }
 
             const cursor = parcelsCollection.find(query)
@@ -266,13 +269,27 @@ async function run() {
         })
 
         app.patch('/parcels/:id/status', async(req, res) => {
-            const { deliveryStatus } = req.body;
+            const { deliveryStatus, riderId } = req.body;
+
             const query = {_id: new ObjectId(req.params.id) }
             const updatedDoc = {
                 $set: {
                     deliveryStatus: deliveryStatus
                 }
             }
+
+            if(deliveryStatus === 'parcel_delivered') {
+                // UPDATE RIDER INFORMATION -->
+                
+                const riderQuery = { _id: new ObjectId(riderId) }
+                const riderUpdatedDoc = {
+                    $set: {
+                        workStatus: 'available'
+                    }
+                }
+                const riderResult = await ridersCollection.updateOne(riderQuery, riderUpdatedDoc);
+            }
+
             const result = await parcelsCollection.updateOne(query, updatedDoc)
             res.send(result);
 
